c5448732051f4bfbb28a0f56601873c73054a997
heimzza <hamzassm18@gmail.com>
2020-10-04 00:17:53 +0300

    ürün detay sayfası

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -11,73 +11,78 @@ class ProductCard extends StatelessWidget {
     this.width = 140,
     this.aspectRatio = 1.02,
     @required this.product,
+    @required this.press,
   }) : super(key: key);
 
   final double width, aspectRatio;
   final Product product;
+  final GestureTapCallback press;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: getproportionateScreenWidth(20)),
-      child: SizedBox(
-        width: getproportionateScreenWidth(width),
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: aspectRatio,
-              child: Container(
-                padding: EdgeInsets.all(getproportionateScreenWidth(20)),
-                decoration: BoxDecoration(
-                  color: kSecondartColor.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(15),
+      child: GestureDetector(
+        onTap: press,
+        child: SizedBox(
+          width: getproportionateScreenWidth(width),
+          child: Column(
+            children: [
+              AspectRatio(
+                aspectRatio: aspectRatio,
+                child: Container(
+                  padding: EdgeInsets.all(getproportionateScreenWidth(20)),
+                  decoration: BoxDecoration(
+                    color: kSecondartColor.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Image.asset(product.images[0]),
                 ),
-                child: Image.asset(product.images[0]),
               ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              product.title,
-              style: TextStyle(color: Colors.black),
-              maxLines: 2,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "${product.price} TL",
-                  style: TextStyle(
-                    fontSize: getproportionateScreenWidth(18),
-                    fontWeight: FontWeight.w600,
-                    color: kPrimaryColor,
-                  ),
-                ),
-                InkWell(
-                  borderRadius: BorderRadius.circular(30),
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.all(getproportionateScreenWidth(8)),
-                    width: getproportionateScreenWidth(28),
-                    height: getproportionateScreenWidth(28),
-                    decoration: BoxDecoration(
-                      color: product.isFavourite
-                          ? kPrimaryColor.withOpacity(0.15)
-                          : kSecondartColor.withOpacity(0.1),
-                      shape: BoxShape.circle,
-                    ),
-                    child: SvgPicture.asset(
-                      "assets/icons/Heart Icon_2.svg",
-                      color: product.isFavourite
-                          ? Color(0xFFFF4848)
-                          : Color(0xFFDBDEE4),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                product.title,
+                style: TextStyle(color: Colors.black),
+                maxLines: 2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "${product.price} TL",
+                    style: TextStyle(
+                      fontSize: getproportionateScreenWidth(18),
+                      fontWeight: FontWeight.w600,
+                      color: kPrimaryColor,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  InkWell(
+                    borderRadius: BorderRadius.circular(30),
+                    onTap: () {},
+                    child: Container(
+                      padding: EdgeInsets.all(getproportionateScreenWidth(8)),
+                      width: getproportionateScreenWidth(28),
+                      height: getproportionateScreenWidth(28),
+                      decoration: BoxDecoration(
+                        color: product.isFavourite
+                            ? kPrimaryColor.withOpacity(0.15)
+                            : kSecondartColor.withOpacity(0.1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset(
+                        "assets/icons/Heart Icon_2.svg",
+                        color: product.isFavourite
+                            ? Color(0xFFFF4848)
+                            : Color(0xFFDBDEE4),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/home/components/popular_products.dart
+++ lib/screens/home/components/popular_products.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/components/product_card.dart';
 import 'package:shop_app/models/Product.dart';
+import 'package:shop_app/screens/details/details_screen.dart';
 
 import '../../../size_config.dart';
 import 'section_title.dart';
@@ -29,6 +30,13 @@ class PopularProducts extends StatelessWidget {
                 demoProducts.length,
                 (index) => ProductCard(
                   product: demoProducts[index],
+                  press: () => Navigator.pushNamed(
+                    context,
+                    DetailsScreen.routeName,
+                    arguments: ProductDetailsArguments(
+                      product: demoProducts[index],
+                    ),
+                  ),
                 ),
               ),
               SizedBox(
